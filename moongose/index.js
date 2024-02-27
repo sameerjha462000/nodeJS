e4e9c3018381c2004cc95978a48c690fc3591e41
Abhishek jha <sameerjha462000@gmail.com>
2024-02-27 16:03:15 +0530

    .

---
+++ moongose/index.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://127.0.0.1:27017/movieApp').then(() => { 
+    console.log('Connection made successfully')
+}).catch(err => { 
+    console.log('Error')
+})
+
+const movieSchema = new mongoose.Schema({
+    title: String,
+    year: Number,
+    score: Number,
+    rating : String
+})
+
+// This will create a new Model
+const Movie = mongoose.model('Movie', movieSchema) // movies
+
+const amadeus = new Movie({ title: 'Amadeus', year: 1986, score: 9.2, rating: 'R' })
+
+amadeus.save()
